24bebdcff7612f59f8f899ccb431e68e280fed5d
Justin Rodriguez <justin.r@me.com>
2023-06-14 08:38:24 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,4 +1,75 @@
-/* Your Code Here */
+function createEmployeeRecord(array){
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+function createEmployeeRecords(array){
+    return array.map(e => createEmployeeRecord(e))
+}
+
+function createTimeInEvent(date){
+    const obj = {}
+
+    obj.type = 'TimeIn'
+    obj.hour = parseInt(date.slice(11, 13) + '00')
+    obj.date = date.slice(0, 10)
+
+    this.timeInEvents.push(obj)
+    return this
+}
+
+function createTimeOutEvent(date){
+    const obj = {}
+
+    obj.type = 'TimeOut'
+    obj.hour = parseInt(date.slice(11, 13) + '00')
+    obj.date = date.slice(0, 10)
+
+    this.timeOutEvents.push(obj)
+    return this
+}
+
+function hoursWorkedOnDate(date){
+    let timeIn = 0
+    let timeOut = 0
+
+    this.timeInEvents.forEach(element => {
+        if (element.date === date){
+            timeIn = element.hour
+        }
+    })
+
+    this.timeOutEvents.forEach(element => {
+        if (element.date === date){
+            timeOut = element.hour
+        }
+    })
+
+    return (timeOut - timeIn) / 100
+}
+
+function wagesEarnedOnDate(date){
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
+function findEmployeeByFirstName(array, firstName){
+    let record = {}
+    array.forEach(element => {
+        if (element.firstName === firstName){
+            record = element
+        }
+    })
+    return record
+}
+
+function calculatePayroll(array){
+    return array.reduce((accum, current) => accum + allWagesFor.call(current), 0)
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
